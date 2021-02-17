e69b5921067a70fe3fa52c42e809c9895d0440e2
Chris Hartgerink <chris@libscie.org>
2021-02-17 10:56:48 +0100

    Prepare for initial release

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# design-colors
+# Design library <img src="https://raw.githubusercontent.com/libscie/design/main/libscie-logomark-1024-square.png" align="right" height="64" />
+
+This repository helps standardize way designs are implemented across liberate science projects. This makes it easier to propagate any changes to the design system, by simply updating this library.
+
+## Usage
+
+```js
+npm install @libscie/design-library
+const libscie = require('@libscie/design-library')
+// If you want to import only specific parts of the design library
+const {
+    colors
+} = require('@libscie/design-library')
+```
+
+## Colors
+
+![Color library cheatsheet](./color-cheatsheet.png)
---
+++ index.js
@@ -0,0 +1,37 @@
+exports.colors = {
+  purple900: '#2C2683',
+  purple700: '#433BB9',
+  purple500: '#574CFA',
+  purple300: '#6C63F1',
+  purple100: '#BEBAFB',
+
+  blue900: '#033C76',
+  blue700: '#0669CD',
+  blue500: '#0580FC',
+  blue300: '#3096FE',
+  blue100: '#9BCDFF',
+
+  green900: '#01564A',
+  green700: '#028A75',
+  green500: '#01A38A',
+  green300: '#04B197',
+  green100: '#6DD6C5',
+
+  yellow900: '#B95900',
+  yellow700: '#DE7412',
+  yellow500: '#FE9100',
+  yellow300: '#F9A331',
+  yellow100: '#FFCC88',
+
+  red900: '#960101',
+  red700: '#B10202',
+  red500: '#E5032C',
+  red300: '#FF4747',
+  red100: '#FFA3A3',
+
+  mono900: '#090909',
+  mono700: '#464646',
+  mono500: '#848484',
+  mono300: '#C0C0C0',
+  white: '#FFFFFF'
+}
